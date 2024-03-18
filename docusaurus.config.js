ba5688d3db91d9029f78efd04623d4f0b2f0263b
Vanshika <vanshikasrivastava.business@gmail.com>
2024-03-19 02:16:03 +0530

    added image for metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: "img/preview-gnosis.png",
+      image: "img/preview-gnosis.jpg",
       announcementBar: {
         id: "support_us",
         content:
@@ -222,8 +222,7 @@ const config = {
         {
           name: "google-site-verification",
           content: "P--3KGPeNoGjwcr2ZM1-m42FLjd8WL_Ly7XWTedX2U4",
-        },
-        { name: "twitter:card", content: "summary_large_image" },
+        }
       ],
     }),
 
