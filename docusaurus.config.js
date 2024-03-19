fd2e9b4001389bdd7a8072837c96067f2a461d19
Vanshika <vanshikasrivastava.business@gmail.com>
2024-03-19 18:56:07 +0530

    minor fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "gnosischain", // Usually your GitHub org/user name.
   projectName: "documentation", // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
